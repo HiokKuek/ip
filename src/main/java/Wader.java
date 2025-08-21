2f372b8ab57889e47f1f941d1d405856e84af11b
HiokKuek <128700697+HiokKuek@users.noreply.github.com>
2025-08-21 15:17:32 +0800

    refactor echo

--- src/main/java/Wader.java
+++ src/main/java/Wader.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 public class Wader {
     public static void main(String[] args) {
-        String horizontal_line = "____________________________________________________________\n";
-        String welcome_message = "Hello! I'm Wader\n" +
-                "What can I do for you?\n";
-        String goodbye_message = "Bye. Hope to see you soon!\n";
+        Wader.printMessages();
+    }
 
-        String wel_message = horizontal_line +
-                welcome_message +
-                horizontal_line;
-        String bybye_message = goodbye_message + horizontal_line;
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.println(wel_message);
+    private static void printMessages() {
+        // Print Welcome Message
+        System.out.println(Messages.getWelcomeMessage());
 
+        // Echo user_input
         String user_input = "";
+        Scanner inputScanner = new Scanner(System.in);
         while (!user_input.equals("bye")) {
             user_input = inputScanner.nextLine();
-            System.out.println(horizontal_line);
-            System.out.println("Added: " + user_input);
-            System.out.println(horizontal_line);
+            System.out.println(Messages.printCustomMessage("Added: " + user_input));
         }
-        System.out.println(bybye_message);
+        inputScanner.close();
+
+        // Print Goodby Message
+        System.out.println(Messages.getGoodbyeMessage());
 
     }
 }
