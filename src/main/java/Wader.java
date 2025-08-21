2cf254c58a7084e4de673985c431c70607b565f5
HiokKuek <128700697+HiokKuek@users.noreply.github.com>
2025-08-21 09:09:39 +0800

    Add Echo functionality

--- src/main/java/Wader.java
+++ src/main/java/Wader.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Wader {
     public static void main(String[] args) {
         String horizontal_line = "____________________________________________________________\n";
@@ -5,12 +7,21 @@ public class Wader {
                 "What can I do for you?\n";
         String goodbye_message = "Bye. Hope to see you soon!\n";
 
-        String message = horizontal_line +
+        String wel_message = horizontal_line +
                 welcome_message +
-                horizontal_line +
-                goodbye_message +
                 horizontal_line;
+        String bybye_message = goodbye_message + horizontal_line;
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println(wel_message);
 
-        System.out.println(message);
+        String user_input = "";
+        while (!user_input.equals("bye")) {
+            user_input = inputScanner.nextLine();
+            System.out.println(horizontal_line);
+            System.out.println("Added: " + user_input);
+            System.out.println(horizontal_line);
+        }
+        System.out.println(bybye_message);
+
     }
 }
